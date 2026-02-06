fe8e3b93fd33d6ac65039567d96e8dc4354a38d9
Noah MacDonald <nmacdonald2113@gmail.com>
2026-02-06 14:49:59 -0500

    Updated Linkedin badge

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LinkedinBadgeComponent } from "../../components/linkedin-badge/linkedin-badge.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [LinkedinBadgeComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
