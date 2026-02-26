de376bfafee4cab447f6282d4d1ae6bbcae654b5
Noah MacDonald <nmacdonald2113@gmail.com>
2026-02-26 15:09:24 -0500

    added dark mode saved state

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,13 +7,28 @@ import { NgIf } from '@angular/common';
   standalone: true,
   imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   isDarkMode = false;
 
   toggleTheme() {
     this.isDarkMode = !this.isDarkMode;
-    document.body.classList.toggle('dark-mode', this.isDarkMode);
+      if (this.isDarkMode) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+    }
+  }
+
+  constructor() {
+    const saved = localStorage.getItem('theme');
+
+    if (saved === 'dark') {
+      this.isDarkMode = true;
+      document.body.classList.add('dark-mode');
+    }
   }
 }
